e80977fab95ce247a7f73a7e38f427b21d536187
tomansvk <tomanka.filip@gmail.com>
2025-05-13 16:11:57 +0200

    fixed index html

--- index.js
+++ index.js
@@ -8,16 +8,22 @@ const app = express();
 // Enable JSON parsing
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 // Serve static files from /public
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 // Register API routes
-const financialAnalysisRoutes = require('./routes/financialAnalysisRoutes');
+const financialAnalysisRoutes = require('./src/routes/financialAnalysisRoutes');
 app.use('/api/financial-analysis', financialAnalysisRoutes);
 
 
 app.listen(PORT, () => {
-  const url = `http://localhost:${PORT}/index.html`;
+  const url = `http://localhost:${PORT}/`;
   console.log(`FinBotics running at ${url}`);
   open(url);  // Open in default browser
 });
